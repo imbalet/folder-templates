55fbaef4d9b53e25e36d52ef951065754b9a2879
imbalet <zhmaevva@gmail.com>
2026-09-05 22:04:06 +0300

    fix: repair development build

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -18,18 +18,6 @@ const options = {
   minify: production,
 };
 
-function copyToVault() {
-  fs.mkdirSync(pluginDir, {
-    recursive: true,
-  });
-
-  fs.copyFileSync("main.js", path.join(pluginDir, "main.js"));
-
-  fs.copyFileSync("styles.css", path.join(pluginDir, "styles.css"));
-
-  fs.copyFileSync("manifest.json", path.join(pluginDir, "manifest.json"));
-}
-
 if (production) {
   await esbuild.build(options);
 
@@ -45,6 +33,18 @@ if (production) {
     "plugins",
     "folder-templates",
   );
+
+  function copyToVault() {
+    fs.mkdirSync(pluginDir, {
+      recursive: true,
+    });
+
+    fs.copyFileSync("main.js", path.join(pluginDir, "main.js"));
+
+    fs.copyFileSync("styles.css", path.join(pluginDir, "styles.css"));
+
+    fs.copyFileSync("manifest.json", path.join(pluginDir, "manifest.json"));
+  }
 
   const context = await esbuild.context(options);
 
